7b4023dd75927e9af6df8b7001aa2b64f3051efe
Gabriel Rosa <154629860+gabrielrosa0110@users.noreply.github.com>
2026-08-13 16:34:32 -0300

    Add files via upload

---
+++ src/main.java
@@ -0,0 +1,28 @@
+public class main {
+    public static void main(String[] args) {
+
+        Zumbi z1 = new Zumbi();
+        Zumbi z2 = new Zumbi();
+
+        z1.nome = "Alanzoka";
+        z1.vida = 250;
+
+        z2.nome = "Juquinha";
+        z2.vida = 250;
+
+
+        z1.mostraVida();
+        z2.mostraVida();
+
+        z1.transfereVida(z2,100);
+
+        System.out.println( z1.nome);
+        System.out.println( "Vida: " + z1.vida);
+
+        System.out.println();
+        System.out.println(z2.nome);
+        System.out.println("Vida: " + z2.vida);
+
+
+    }
+}
